56694af134c7becf48feb017ed67e6fe3487003f
Erik L. Underbjerg <elu@dawnhealth.com>
2022-06-16 11:03:27 +0200

    Syntax error fix. Try to locate the bundle of gherkin-language.json when Cucumberish is included as a Swift package

--- Cucumberish/Dependencies/Gherkin/GHGherkinDialectProvider.m
+++ Cucumberish/Dependencies/Gherkin/GHGherkinDialectProvider.m
@@ -55,7 +55,7 @@
     NSBundle * gherkinLanguagesBundle = [NSBundle bundleWithPath: [[NSBundle bundleForClass:[self class]] pathForResource: @"GherkinLanguages" ofType: @"bundle"]];
     if(gherkinLanguagesBundle == nil){
         // Look for a Cucumberish Swift Package resource bundle
-        gherkinLanguagesBundle = [[NSBundle bundleForClass:[self class]] pathForResource: @"Cucumberish_Cucumberish" ofType: @"bundle"]];
+        gherkinLanguagesBundle = [[NSBundle bundleForClass:[self class]] pathForResource: @"Cucumberish_Cucumberish" ofType: @"bundle"];
     }
     if(gherkinLanguagesBundle == nil){
         gherkinLanguagesBundle = [NSBundle bundleForClass:[self class]];
